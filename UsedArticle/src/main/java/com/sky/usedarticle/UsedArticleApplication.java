0096667e5eb903c155455170229fd1260f6cbbf7
jincheolyong <155132415+jincheolyong@users.noreply.github.com>
2024-07-05 18:09:42 +0900

    .

---
+++ UsedArticle/src/main/java/com/sky/usedarticle/UsedArticleApplication.java
@@ -0,0 +1,13 @@
+package com.sky.usedarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedArticleApplication.class, args);
+    }
+
+}
---
+++ UsedArticle/src/test/java/com/sky/usedarticle/UsedArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.usedarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
